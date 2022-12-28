f1a7fe2cfe516201e36644e3a4bfeb8e4aa5efdb
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-12-28 15:29:10 +0300

    Add routing and versioning for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :bands
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :bands
+      resources :members
+    end
+  end  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
